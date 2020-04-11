22117a89f03a5ae94a04d79b103dea76a6f62434
Alexander Parvanov <47386361+YYx00xZZ@users.noreply.github.com>
2020-04-11 17:06:35 +0300

    .

--- bitbci/bitbci.py
+++ bitbci/bitbci.py
@@ -1,5 +1,20 @@
+import pandas as pd
+from data.config_reader import configReader
+from data.data_preprocess import populate_NaN, populate_ids, filter_events
+
+
 def main():
-    pass
+    config = configReader()
+
+    df = pd.read_csv(config['input_file_url'], names=config['names'], index_col=False, header=None, sep=',',skiprows=1, dtype={'EventId' : str})
+    # зареждаме входния csv файл от Github
+    
+    dataframe = populate_NaN(df)
+    data = filter_events(dataframe)
+    print(type(data))
+    print(data.shape)
+    print(data.EventId.unique())
+
 
 if __name__ == '__main__':
-    print(__name__)
+    main()
